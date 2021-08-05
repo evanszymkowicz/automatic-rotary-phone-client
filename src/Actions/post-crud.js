b8210329a442fc20f8718f161a878d8a1b8943b8
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2021-08-05 16:11:57 -0400

    fixing actions

--- src/Actions/post-crud.js
+++ src/Actions/post-crud.js
@@ -1,4 +1,7 @@
 import {API_BASE_URL} from '../config';
+import { crudError } from './index';
+import { normalizeResponseErrors } from './utils';
+import { SubmissionError } from 'redux-form';
 
 //  POST/PUT Actions
 export const SUBMIT_POST_REQUEST = "SUBMIT_POST_REQUEST";
@@ -14,12 +17,7 @@ export const submitPostSuccess = (post, currentUserId, postId) => ({
     postId
 })
 
-export const CRUD_ERROR = "CRUD_ERROR";
-export const crudError = () => ({
-  type: CRUD_ERROR,
-})
-
-export const submitPost = (values, currentUserId, postId) => dispatch =>{
+export const submitPost = (values, currentUserId, postId) => (dispatch, getState) =>{
     //  use the same form for editing existing forms and creating a new one
     const method = postId ? "PUT" : "POST";
     const path = postId ? `${API_BASE_URL}/posts/${currentUserId}/${postId}` : `${API_BASE_URL}/posts/${currentUserId}`; 
@@ -29,21 +27,35 @@ export const submitPost = (values, currentUserId, postId) => dispatch =>{
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`
         },
         body: JSON.stringify(values)
     })
-    .then(res => {
-        if (!res.ok) {
-            return Promise.reject(res.statusText);
-        }
-        return res.json();
-    }).then(post => {
-        console.log('in actions, got back post:', post);
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(post => {
         dispatch(submitPostSuccess(post, currentUserId, postId));
     }).catch(err => {
-        dispatch(crudError(err));
+        dispatch(crudError("An error has occured. Please try again."));
+        const {message, location, status} = err;
+        if (status === 400) {
+            // Convert errors into SubmissionErrors for Redux Form
+            return Promise.reject(
+                new SubmissionError({
+                    [location]: message
+                })
+            );
+        }
+        else{
+            return Promise.reject(
+                new SubmissionError({
+                    _error: 'Unable to create post, please try again',
+                })
+            );
+        }
     });
 }
+
 
 //  Delete POST
 export const DELETE_POST_SUCCESS = 'DELETE_POST_SUCCESS';
@@ -58,23 +70,22 @@ export const deletePostRequest = () => ({
     type: DELETE_POST_REQUEST,
 })
 
-export const deletePost = (currentUserId, postId) => dispatch =>{
+export const deletePost = (currentUserId, postId) => (dispatch, getState) =>{
     dispatch(deletePostRequest());
+    const authToken = getState().auth.authToken;
     fetch(`${API_BASE_URL}/posts/${currentUserId}/${postId}`, { 
         method: "DELETE",
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`
         }
     })
-    .then(res => {
-        if (!res.ok) {
-            return Promise.reject(res.statusText);
-        }
-        console.log('successful deleting');
+    .then(res => normalizeResponseErrors(res))
+    .then(() => {
         dispatch(deletePostSuccess(currentUserId, postId));
     })
     .catch(err => {
-        dispatch(crudError(err));
+        dispatch(crudError("An error has occured. Please try refreshing!"));
     });
-  }
+}
--- src/Actions/reminder-crud.js
+++ src/Actions/reminder-crud.js
@@ -1,9 +1,12 @@
 import { API_BASE_URL } from '../config';
+import {  normalizeResponseErrors } from './utils';
+import {  crudError } from './index';
 
+// POST and PUT Actions
 export const SUBMIT_REMINDER_REQUEST = "SUBMIT_REMINDER_REQUEST";
 export const submitReminderRequest = () => ({
   type: SUBMIT_REMINDER_REQUEST,
-})
+});
 
 export const SUBMIT_REMINDER_SUCCESS = "SUBMIT_REMINDER_SUCESS";
 export const submitReminderSuccess = (reminder, currentUserId, reminderId) => ({
@@ -13,38 +16,30 @@ export const submitReminderSuccess = (reminder, currentUserId, reminderId) => ({
   reminderId
 })
 
-export const CRUD_ERROR = "CRUD_ERROR";
-export const crudError = () => ({
-  type: CRUD_ERROR,
-})
-
-export const submitReminder = (values, currentUserId, reminderId) => dispatch => {
+export const submitReminder = (values, currentUserId, reminderId) => (dispatch, getState) => {
   const method = reminderId ? "PUT" : "POST";
   const path = reminderId ? `${API_BASE_URL}/reminders/${currentUserId}/${reminderId}` : `${API_BASE_URL}/reminders/${currentUserId}`;
-
   dispatch(submitReminderRequest());
-  fetch(path, {
-      method: method,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(values)
-    })
-    .then(res => {
-      if (!res.ok) {
-        return Promise.reject(res.statusText);
-      }
-      return res.json();
-    }).then(reminder => {
-      console.log('in actions, got back userfile:', reminder);
-      dispatch(submitReminderSuccess(reminder, currentUserId, reminderId));
-    }).catch(err => {
-      dispatch(crudError(err));
-    });
+  const authToken = getState().auth.authToken;
+  fetch ( path, {
+    method: method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authToken}`
+    },
+    body: JSON.stringify(values)
+  })
+  .then(res => normalizeResponseErrors(res))
+  ,then(res => res.json())
+  .then(reminder => {
+    dispatch(submitReminderSuccess(reminder, currentUserId, reminderId));
+  }).catch(err => {
+    dispatch(crudError("An error has occured. Please try again."));
+  });
 }
 
-/* DELETE */
+//  DELETE
 export const DELETE_REMINDER_SUCCESS = 'DELETE_REMINDER_SUCCESS';
 export const deleteReminderSuccess = (currentUserId, reminderId) => ({
   type: DELETE_REMINDER_SUCCESS,
@@ -57,24 +52,22 @@ export const deleteReminderRequest = () => ({
   type: DELETE_REMINDER_REQUEST,
 })
 
-export const deleteReminder = (currentUserId, reminderId) => dispatch => {
-  console.log('in delete reminder action, deleting reminder with id', reminderId, 'in User with id', currentUserId);
+export const deleteReminder = (currentUserId, reminderId) => (dispatch, getState) => {
   dispatch(deleteReminderRequest());
-  fetch(`${API_BASE_URL}/reminders/${currentUserId}/${reminderId}`, {
-      method: "DELETE",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }
+    const authToken = getState().auth.authToken;
+    fetch(`${API_BASE_URL}/reminders/${currentUserId}/${reminderId}`, { 
+        method: "DELETE",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`
+        }
     })
-    .then(res => {
-      if (!res.ok) {
-        return Promise.reject(res.statusText);
-      }
-      // console.log('deleted successfully');
-      dispatch(deleteReminderSuccess(currentUserId, reminderId));
+    .then(res => normalizeResponseErrors(res))
+    .then(() => {
+        dispatch(deleteReminderSuccess(currentUserId, reminderId));
     })
     .catch(err => {
-      dispatch(crudError(err));
+        dispatch(crudError("An error has occured. Please try refreshing!"));
     });
-}
+  }
